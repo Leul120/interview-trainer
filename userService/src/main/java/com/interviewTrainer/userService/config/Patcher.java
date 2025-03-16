2251065b0782e62178454b38b28e9ba5779def0b
Leul120 <140542389+Leul120@users.noreply.github.com>
2025-03-16 05:28:48 -0700

    Update Patcher.java

--- userService/src/main/java/com/interviewTrainer/userService/config/Patcher.java
+++ userService/src/main/java/com/interviewTrainer/userService/config/Patcher.java
@@ -9,16 +9,27 @@ import java.lang.reflect.Field;
 @Component
 public class Patcher {
 
-    public static void userPatcher(User existingUser, UserRequest incompleteUser) throws  IllegalAccessException {
-        Class<?> userClass= User.class;
-        Field[] userFields= userClass.getDeclaredFields();
-        for(Field field:userFields){
+    public static void userPatcher(User existingUser, UserRequest incompleteUser) throws IllegalAccessException {
+        Class<?> userClass = User.class;
+        Field[] userFields = userClass.getDeclaredFields();
+
+        for (Field field : userFields) {
+            if (field.getName().equals("id")) { // Skip id field
+                continue;
+            }
             field.setAccessible(true);
-            Object value=field.get(incompleteUser);
-            if (value!=null){
-                field.set(existingUser,value);
+            Object value = null;
+            try {
+                value = field.get(incompleteUser);
+            } catch (IllegalArgumentException e) {
+                // This happens when `UserRequest` doesn't have the field
+                continue;
+            }
+            if (value != null) {
+                field.set(existingUser, value);
             }
             field.setAccessible(false);
         }
     }
+
 }
